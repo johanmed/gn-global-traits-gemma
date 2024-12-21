6245679eb44ea601d8381b4d2d0a8184cc1ab44b
Johannes Medagbe <johanmedagbe@gmail.com>
2024-12-21 10:59:03 +0300

    multi_pheno_json2one_pheno_bimbam.py modified and working, results attached

--- scripts/multi_pheno_json2one_pheno_bimbam.py
+++ scripts/multi_pheno_json2one_pheno_bimbam.py
@@ -5,7 +5,7 @@
 import os
 import re
 
-list_json_files=[os.path.join('../processed_data', file) for file in os.listdir('../processed_data') if 'pheno' in file]
+list_json_files=[os.path.join('../processed_data/json_files', file) for file in os.listdir('../processed_data/json_files') if 'pheno' in file]
 
 
 def process_json_bimbam(json_filename, bimbam_filename):
@@ -26,34 +26,41 @@ def process_json_bimbam(json_filename, bimbam_filename):
         strain_found=re.search('sample_name_2', y)
         
         if not (strain_found==None):
-            value_found=re.search('value', json_contents[x+1])
+        
+            value_found1=re.search('value', json_contents[x+1]) # trait value expected to be on next line
+            value_found2=re.search('value', json_contents[x+2]) # or trait value on second next line
             
             strain_extract= "".join(char for char in strain_found.string[-10:-1] if char.isalnum())
-            print('strain extract is ', strain_extract)
+            #print('strain extract is ', strain_extract)
             
-            value_extract="".join(char for char in value_found.string[-10:-1] if char.isdigit() or char=='.')
-            print('value extract is ', value_extract)
+            if not (value_found1==None):
+                value_extract="".join(char for char in value_found1.string[-10:-1] if char.isdigit() or char=='.')
+            elif not(value_found2==None):
+                value_extract="".join(char for char in value_found2.string[-10:-1] if char.isdigit() or char=='.')
+                
+            #print('value extract is ', value_extract)
             
             container.append([strain_extract, value_extract])
     
-    print('container is ', container)
-    
-    bimbam1=open(bimbam_filename, 'a')
+    #print('container is ', container)
     
     bimbam2=open(bimbam_filename)
     bimbam2_contents=bimbam2.read()
     bimbam2.close()
     
     for a in container:
-        bimbam1=open(bimbam_filename, 'a')
         if a[0] in bimbam2_contents:
+            bimbam1=open(bimbam_filename, 'w')
+            
             old_string=(re.search(f'{a[0]}.*\n', bimbam2_contents).group())[:-1]
-            print('old string is ', old_string)
+            #print('old string is ', old_string)
             new_string=bimbam2_contents.replace(old_string, f'{old_string}, {a[1]}')
-            print('new string is ', new_string)
+            #print('new string is ', new_string)
             
             bimbam1.write(f'{new_string}\n')
+            bimbam1.close()
             
+        bimbam1=open(bimbam_filename, 'a')
         bimbam1.write(f'{a[0]}, {a[1]}\n')
         bimbam1.close()
     
@@ -72,15 +79,11 @@ def contains_phenotype_data(json_filename):
         return False
         
 
-f=open('../processed_data/project_phenotype_file.bimbam', 'a')
-f.write(f'strain_id, trait_value\n') # add header for clarity
-f.close()
-
 
 for jsonf in list_json_files:
-    print(f'Processing {jsonf}')
+    #print(f'Processing {jsonf}')
     if contains_phenotype_data(jsonf):
-        print(f'File {jsonf} contains data and can go for actual processing')
+        #print(f'File {jsonf} contains data and can go for actual processing')
         process_json_bimbam(jsonf, '../processed_data/project_phenotype_file.bimbam')
     
 
