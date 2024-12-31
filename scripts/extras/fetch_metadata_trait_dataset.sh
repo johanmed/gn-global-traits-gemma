1c7eefd6748d9578d64d03c9c946fb31ff52a6b9
Johannes Medagbe <johanmedagbe@gmail.com>
2024-12-31 11:47:34 +0300

    Info on trait and dataset added to fetch_metadata for generation of informed metadata file

--- scripts/extras/fetch_metadata_trait_dataset.sh
+++ scripts/extras/fetch_metadata_trait_dataset.sh
@@ -5,6 +5,8 @@
 for i in {1..19544}; do
         dataset_id=$(cut -d, -f1 ../../processed_data/list_dataset_name_trait_id.csv | tail -n +3 | head -n $i | tail -n 1)
         trait_id=$(cut -d, -f2 ../../processed_data/list_dataset_name_trait_id.csv | tail -n +3 | head -n $i | tail -n 1)
+        echo Trait id: ${trait_id} >> ../../processed_data/metadata_phenotype_info_file.json
+        echo Dataset id : ${dataset_id} >> ../../processed_data/metadata_phenotype_info_file.json
         curl https://genenetwork.org/api/v_pre1/trait/${dataset_id}/${trait_id} >> ../../processed_data/metadata_phenotype_info_file.json
 done
 
