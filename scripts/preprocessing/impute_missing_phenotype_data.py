de9a4ed272c546179596612c929b7da71e4453cf
Johannes Medagbe <johanmedagbe@gmail.com>
2025-01-01 20:28:02 +0300

    Imputation of missing values improved using IterativeImputer with sampling to create variability in phenotype values by column

--- scripts/preprocessing/impute_missing_phenotype_data.py
+++ scripts/preprocessing/impute_missing_phenotype_data.py
@@ -8,54 +8,52 @@
 import pandas as pd
 import numpy as np
 
-trimmed_BXD_data=pd.read_csv('../../processed_data/project_fully_trimmed_phenotype_file.bimbam', header=None)
-trimmed_data=trimmed_BXD_data.copy()
-
-#print('trimmed data looks like \n', trimmed_data.head())
+# Read trait names in order
 
 f1=open('../../processed_data/order_trait_names_phenotype_file.csv', 'r')
-order_trait_names=f1.readline().split(',') # get an array of the trait names in the same order as the phenotype file used
+order_traits=f1.readline().split(',') # get an array of the trait names in the same order as the phenotype file used
 #print('List of trait names is: ', order_trait_names)
 #print(f'List of trait names has {len(order_trait_names)} elements')
 f1.close()
 
+# Read phenotype data in right order
 
-from sklearn.impute import KNNImputer
+trimmed_BXD_data=pd.read_csv('../../processed_data/project_fully_trimmed_phenotype_file.bimbam', header=None, names=order_traits)
+trimmed_data=trimmed_BXD_data.copy()
 
-def impute_missing_column(column_data):
-    imputer=KNNImputer() 
-    return imputer.fit_transform(column_data)
+print('trimmed data looks like \n', trimmed_data.head())
+
+
+
+from sklearn.experimental import enable_iterative_imputer
+from sklearn.impute import IterativeImputer
+
+def impute_missing_values(dataset):
+    """
+    Use all features of dataset to impute iteratively missing values in columns with at least one non-missing values
+    Return random imputed values drawn from Gaussian prediction of default estimator > 0.0
+    """
+    imputer=IterativeImputer(sample_posterior=True, random_state=2024, min_value=0.0) 
+    new_dataset=imputer.fit_transform(dataset)
+    features_out=imputer.get_feature_names_out()
+    return new_dataset, features_out
 
     
+# Proceed to imputation
 
-def impute_missing_dataset(dataset, order_traits):
-    new_dataset={}
-    new_order_traits=[]
-    for col in dataset.columns:
-        #print(f'the index is {ind} and the column name {col}')
-        data=dataset[col]
-        if data.notna().any(): # only take columns with data for imputation 
-            imputed_col=impute_missing_column(pd.DataFrame(data))
-            #print('imputed col is \n', imputed_col)
-            new_col=[i[0] for i in imputed_col]
-            #print('new col is \n', new_col)
-            
-            new_dataset[col]=new_col
-            new_order_traits.append(order_traits[col])
-        
-    imputed_dataset=pd.DataFrame(new_dataset)
-    
-    return imputed_dataset, new_order_traits
+imputed_data, new_order_traits=impute_missing_values(trimmed_data)
+imputed_BXD_data=pd.DataFrame(imputed_data)
+print('Imputed data is \n', imputed_BXD_data.head())
+#print('New order trait names is: ', new_order_traits)
+#print('Number of traits kept after modification', len(new_order_traits))
 
-imputed_BXD_data, order_traits=impute_missing_dataset(trimmed_data, order_trait_names)
-#print('Imputed data is \n', imputed_BXD_data.head())
-#print('New order trait names is: ', order_traits)
-#print('Number of traits kept after modification', len(order_traits))
+# Save new dataset
 
 imputed_BXD_data.to_csv("../../processed_data/project_imputed_phenotype_file.bimbam", index=False, header=False)
 
 
 # Save the new order of the trait names
+
 f2=open('../../processed_data/modified_order_trait_names_phenotype_file.csv', 'w')
 f2.write(','.join(order_traits))
 f2.close()
